62c3015c79ce31ee6eae07a3b1544441a8d6dcd0
Schmarfy <o.vanzijverden@gmail.com>
2018-01-26 16:10:04 +0100

    Change products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,9 @@
 class ProductsController < ApplicationController
-
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find.params([:product_id])
+    @product = Product.find.params([:id])
   end
-
 end
